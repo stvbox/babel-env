4d66100dd78f45e048f729daa94e841a4fa2677a
Переверзев Сергей Михайлович <stvbox@yandex.ru>
2018-07-02 09:37:00 +0300

    init

---
+++ src/script.js
@@ -0,0 +1,8 @@
+import Test from './test.js'
+
+
+console.log(Test.getText())
+
+let [firstName, lastName] = ["Илья", "Кантор"];
+
+console.log(firstName + ' >> ' + lastName)
---
+++ src/test.js
@@ -0,0 +1,5 @@
+export default  {
+    getText() {
+        return 'Пробный текст!!!'
+    }
+}
